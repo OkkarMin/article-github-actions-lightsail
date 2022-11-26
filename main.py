0056c67bfdcab34452960243f57a46351b9cf69c
OkkarMin <OMIN001@e.ntu.edu.sg>
2022-11-26 21:14:07 +0800

    refactor: remove unused code

--- main.py
+++ main.py
@@ -17,7 +17,3 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.post("/items/{item_id}")
 def create_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-# q: how to use sed to replace : with -
-# sed -i 's/:/-/g' main.py
